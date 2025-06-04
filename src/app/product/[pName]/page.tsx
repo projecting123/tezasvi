448e5925c7106e958cb117c9c8087938e3f070a5
Ankur Rajbongshi <dekalasit@gmail.com>
2025-06-04 23:01:36 +0530

    2nd commit

--- src/app/product/[pName]/page.tsx
+++ src/app/product/[pName]/page.tsx
@@ -3,7 +3,6 @@ import { Flex } from "@radix-ui/themes"
 
 export default async function page({ params, searchParams }: { params: Promise<{ pName: string }>, searchParams: Promise<{ pid: string }> }) {
   const { pName } = await params
-  const { pid } = await searchParams
   const formattedName = formatString(pName);
   return (
     <Flex justify={'center'} align={'center'}>
